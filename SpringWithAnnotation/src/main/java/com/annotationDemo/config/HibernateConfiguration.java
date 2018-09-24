3c8f1c9c1c9fdb2215deabc615b37cc1e43f02e2
YASH\dharmendra.biswal <dharmendra.biswal@yash.com>
2018-09-24 10:49:27 +0530

    warnings removed

--- SpringWithAnnotation/src/main/java/com/annotationDemo/config/HibernateConfiguration.java
+++ SpringWithAnnotation/src/main/java/com/annotationDemo/config/HibernateConfiguration.java
@@ -2,7 +2,6 @@ package com.annotationDemo.config;
 
 import java.util.Properties;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
